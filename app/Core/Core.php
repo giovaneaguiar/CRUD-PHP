f51196bd462209a5a9ddaf034b808f09f2a39316
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-21 18:22:40 -0300

    Ajustando Core e Controllers, ou seja, rotas.

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -1,9 +1,33 @@
 <?php
 
-class Core 
+class Core
 {
-    public function start($urlGet){
+    public function start($urlGet)
 
-        $pagina = $urlGet['pagina'];
+    {
+        $acao = 'index';
+
+        if (isset($urlGet['pagina'])) {
+            //se existir a variavel get no parametro pagina.
+            $controller = ucfirst($urlGet['pagina'] . 'Controller');
+            //ucfirst - deixar primeira letra maiuscula
+            //o que está acontecendo aqui - alterando para ter uma rota correta.
+            //exemplo: indo para /home voce entra em HomeController.
+        } else {
+            $controller = 'HomeController';
+            //valor da controller = homecontroller
+        }
+
+
+
+
+        if (!class_exists($controller)) {
+            //se não existir a classe chamada $pagina.
+            $controller = 'ErroController';
+        }
+
+        call_user_func_array(array(new $controller, $acao), array());
+        //chama método de forma dinamica, nesse caso chamou o function index()
+        //em HomeController.php.
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
 require_once 'app/Core/Core.php';
+require_once 'app/Controller/HomeController.php';
+require_once 'app/Controller/ErroController.php';
 
 $template = file_get_contents('app/template/estrutura.html');
 
+ob_start();
 $core = new Core;
 $core->start($_GET);
+$saida = ob_get_contents();
+//pega o conteúdo e joga na variável saída.
+ob_end_clean();
 
-echo $template;
+ $templatePronto = str_replace('{{area_dinamica}}',$saida, $template);
+//carregar o conteúdo da variável template, ou seja
+//estou buscando o html do projeto.
+echo $templatePronto;
