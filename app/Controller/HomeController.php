e7b4886360272f87cf85b038506e531dc0b93796
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-21 19:51:49 -0300

    Twig para acessar o html de maneira inteligente.

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,7 +8,17 @@ class HomeController
         //echo 'Home'
         try {
             $colecPostagens = Postagem::selecionaTodos();
-            var_dump($colecPostagens);
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            $template = $twig->load('home.html');
+
+            $parametros = array();
+            $parametros['postagens'] = $colecPostagens;
+
+            $conteudo = $template->render($parametros);
+            //renderizar a View.
+            echo $conteudo;
+            //mostrar html.
         } catch (Exception $e) {
             echo $e->getMessage();
         }
