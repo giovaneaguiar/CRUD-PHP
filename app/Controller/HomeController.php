422c23d6a287bbac5dda9da23764ee494ce8946f
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-21 20:27:08 -0300

    Finaliza integração: View/Twig. Código Limpo.

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,9 +8,12 @@ class HomeController
         //echo 'Home'
         try {
             $colecPostagens = Postagem::selecionaTodos();
+            
             $loader = new \Twig\Loader\FilesystemLoader('app/View');
             $twig = new \Twig\Environment($loader);
             $template = $twig->load('home.html');
+            //twig serve para alterar o html sem usar php no codigo. ou seja
+            //não mistura .php na file home.html.
 
             $parametros = array();
             $parametros['postagens'] = $colecPostagens;
