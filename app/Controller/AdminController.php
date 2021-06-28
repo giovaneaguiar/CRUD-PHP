ff3c7f84d65e213522b979fabf91153c3616b10c
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-28 12:32:09 -0300

    Finaliza UPDATE do CRUD.

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -56,4 +56,52 @@
             }
 
         }
+
+        public function change($paramId){
+
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            $template  = $twig->load('update.html');
+
+            $post = Postagem::selecionaPorId($paramId);
+
+            $parametros = array();
+            $parametros['id'] = $post->id;
+            $parametros['titulo'] = $post->titulo;
+            $parametros['conteudo'] = $post->conteudo;
+
+            $conteudo = $template->render($parametros);
+            echo $conteudo;
+
+        }
+
+        public function update(){
+
+            
+            try {
+                Postagem::update($_POST);
+                echo '<script>
+                alert("Publicação Alterada com sucesso!");
+                </script>';
+                echo '<script>
+                location.href="http://localhost/CRUD-PHP/?pagina=admin&metodo=index"
+                </script>';
+
+            }
+
+            catch(Exception $e){
+
+                echo '<script>
+                alert("' .$e->getMessage(). '");
+                </script>';
+                echo '<script>
+                location.href="http://localhost/CRUD-PHP/?pagina=admin&metodo=change&id='.$_POST['id'].'"
+                </script>';
+            }
+
+            
+            
+
+
+        }
 	}
